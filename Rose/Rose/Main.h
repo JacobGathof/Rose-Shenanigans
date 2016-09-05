0c76522455feddb162ffdfaba872c101b19a2999
Jake From State Farm <jkgathof@gmail.com>
2016-09-04 22:32:22 -0400

    Window

--- Rose/Rose/Main.h
+++ Rose/Rose/Main.h
@@ -1,8 +1,61 @@
 #pragma once
+#include "GL\glew.h"
+#include "GLFW\glfw3.h"
+
 #include <iostream>
 
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
 int main() {
-	std::cout << "Hello World" << std::endl;
-	std::cout << "Another Line" << std::endl;
-	std::cout << "Jake doesn't know what he's doing" << std::endl;
+
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+
+	GLFWwindow * window = glfwCreateWindow(800, 800, "Default Title", 0, 0);
+	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, key_callback);
+
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (err != GLEW_OK)
+		std::cout << "Error:" << glewGetErrorString(err);
+
+
+	glClearColor(1, 1, 1, 1);
+
+	glfwSetTime(0);
+
+
+	while (!glfwWindowShouldClose(window)) {
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+
+
+	}
+
+	glfwTerminate();
+
+	return 0;
+
 }
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+
+
+	if (action == GLFW_PRESS && key == GLFW_KEY_ESCAPE) {
+		glfwSetWindowShouldClose(window, true);
+		return;
+	}
+
+}
+
