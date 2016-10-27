42093437ffff4db7470f32f05c1d69089beb4f4e
stormin365 <moormaet@rose-hulman.edu>
2016-10-27 16:05:22 -0400

    started weapon

--- Rose/Rose/Weapon.h
+++ Rose/Rose/Weapon.h
@@ -2,20 +2,26 @@
 #include "Entity.h"
 #include "Vector2f.h"
 #include "NPC.h"
+#include "Gem.h"
 class Weapon : public Entity
 {
 public:
 	Weapon();
 	~Weapon();
-	Weapon(int dmg, int critdmg, int percent, Vector2f pos, std::string texName);
+	Weapon(int dmg, int critdmg, int percent, Vector2f pos, int slotNum, std::string texName);
 
 	std::string texture;
 	//Vector2f* playerpos;
 	int damage;
 	int critdamage;
 	int critpercent;
+	std::vector<Gem> slots;
+	int NumSlots;
+
 	bool attacking = false;
 
+	std::vector<int> GetBuffs();
+	Gem addGem(int index, Gem gem);
 	bool attack(NPC obj, Vector2f pos, int dir);
 };
 
