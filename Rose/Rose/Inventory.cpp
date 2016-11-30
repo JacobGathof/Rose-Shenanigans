0bdd97f4f5495d9463f4801475b1ead7320cf515
stormin365 <moormaet@rose-hulman.edu>
2016-11-30 00:25:37 -0500

    Inventory grid made

--- Rose/Rose/Inventory.cpp
+++ Rose/Rose/Inventory.cpp
@@ -13,30 +13,89 @@ Inventory::~Inventory()
 {
 }
 
+void Inventory::BuildPages()
+{
+	Text blank = Text(Vector2f(0, 0), "", Vector2f(textSize, textSize));
+	Text object = Text(Vector2f(0, 0), "Get Object", Vector2f(textSize, textSize));
+	Text temppage[100] = {
+		blank, Text(Vector2f(0,0), "STATS", Vector2f(textSize,textSize)), blank, blank, Text(Vector2f(0,0), "INVENTORY", Vector2f(textSize,textSize)), blank, blank, blank, Text(Vector2f(0,0), "MENU", Vector2f(textSize,textSize)), blank,
+		blank, blank, object, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, object, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, object, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, object, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, object, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, object, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, object, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, object, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, object, blank, blank, blank, blank, blank, blank, blank
+	};
+	for (int i = 0; i < 100; i++) {
+		page1[i] = temppage[i];
+	}
+	Text temppage2[100] = {
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank
+	};
+	for (int i = 0; i < 100; i++) {
+		page2[i] = temppage2[i];
+	}
+	Text temppage3[100] = {
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank,
+		blank, blank, blank, blank, blank, blank, blank, blank, blank, blank
+	};
+	for (int i = 0; i < 100; i++) {
+		page3[i] = temppage3[i];
+	}
+}
+
 int Inventory::Display(int health, int mana, int xp, Vector2f pos)
 {
 	if (!initialized) {
-		tab1 = Text(Vector2f(-45, 55), "Stats", Vector2f(5, 5));
-		tab2 = Text(Vector2f(-15, 55), "Inventory", Vector2f(5, 5));
-		tab3 = Text(Vector2f(30, 55), "Skills", Vector2f(5, 5));
+		BuildPages();
+
 		BackDrop = Object(Vector2f(0, 0), Vector2f(128, 128), "Inventory");
 		initialized = true;
 	}
-	BackDrop.position = pos - Vector2f(48,48);
+	BackDrop.position = pos - Vector2f(48, 48);
 	BackDrop.draw();
-	tab1.draw();
-	tab2.draw();
-	tab3.draw();
-	switch (page)
-		case (0) : 
-			
-
-		case (1) :
-			for (int i = 0; i < weapons.size(); i++) {
-				weapons.at(i).draw();
+	int line = 0;
+	switch (page) {
+		case (0) :
+			line = 0;
+			for (int i = 0; i < 100; i++) {
+				if (page1[i].data == "STATS") {
+					std::cout << line << std::endl;
+				}
+				page1[i].position.x = -60 + ((i%10) * 12);
+				page1[i].position.y = 60 - (line * 12);
+				page1[i].draw();
+				if (i % 10 == 0 && i != 0) {
+					
+					line++;
+				}
 			}
-			
-
+			break;
+		case (1) :
+				for (int i = 0; i < weapons.size(); i++) {
+					weapons.at(i).draw();
+				}
+	};
 	return 0;
 }
 
@@ -54,3 +113,4 @@ void Inventory::discard(Weapon weapon)
 		}
 	}
 }
+
